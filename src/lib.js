162843193c9a5de82116f1576ec4867bd53d4470
lif3ng <lif3ng@icloud.com>
2019-11-30 14:13:52 +0800

    add layout

--- src/lib.js
+++ src/lib.js
@@ -2,9 +2,26 @@ import Pagination from "./components/Pagination";
 import Icon from "./components/Icon";
 import Button from "./components/Button";
 import Input from "./components/form/Input";
+import Container from "./components/layout/Container.vue";
+import Header from "./components/layout/Header.vue";
+import HeaderContainer from "./components/layout/HeaderContainer.vue";
+import Footer from "./components/layout/Footer.vue";
+import Sider from "./components/layout/Sider.vue";
+import Main from "./components/layout/Main.vue";
 import "./style/index.styl";
 
-const components = [Button, Icon, Input, Pagination];
+const components = [
+  Button,
+  Icon,
+  Input,
+  Pagination,
+  Container,
+  Header,
+  HeaderContainer,
+  Footer,
+  Sider,
+  Main
+];
 
 const globalPrefix = prefix;
 
