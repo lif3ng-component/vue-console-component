b5af6f90c0bd738ab5548dfc51101e8546b0af3f
lif3ng <lif3ng@icloud.com>
2020-01-02 11:10:46 +0800

    update iconfont

--- docs/.vuepress/demos/demo-icon.vue
+++ docs/.vuepress/demos/demo-icon.vue
@@ -1,24 +1,53 @@
 <template>
   <div>
-    <div v-for="(type,index) in iconList" :key="index" class="item">
+    <div v-for="(type, index) in iconList" :key="index" class="item">
       <d-icon :type="type" />
-      <span>{{type}}</span>
+      <span>{{ type }}</span>
     </div>
   </div>
 </template>
 <script>
 export default {
-  data(){
+  data() {
     return {
-      iconList:["key","mail","search","point","arrowdown","arrowup","arrowleft","arrowright","check","idcard","mobile","dashboard","plus","caret-up","caret-down","minus","delete","setting","poweroff","down","up","left","right","copy","user","logout","close"]
-    }
+      iconList: [
+        "server",
+        "key",
+        "mail",
+        "search",
+        "point",
+        "arrowdown",
+        "arrowup",
+        "arrowleft",
+        "arrowright",
+        "check",
+        "idcard",
+        "mobile",
+        "dashboard",
+        "plus",
+        "caret-up",
+        "caret-down",
+        "minus",
+        "delete",
+        "setting",
+        "poweroff",
+        "down",
+        "up",
+        "left",
+        "right",
+        "copy",
+        "user",
+        "logout",
+        "close"
+      ]
+    };
   }
-}
+};
 </script>
 <style lang="stylus" scoped>
 .item
   display: inline-block
-  padding: 20px 0 
+  padding: 20px 0
   box-sizing border-box
   line-height: 30px
   font-size: 16px
