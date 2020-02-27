7722282a51dff797600aca19bc4742ca5ff889fa
lif3ng <lif3ng@icloud.com>
2020-02-27 22:25:07 +0800

    feat: use tippy.js refactor v-popper

--- src/components/popper/popper.js
+++ src/components/popper/popper.js
@@ -1,16 +1,12 @@
-import Popper from "popper.js";
-import { throttle } from "lodash/fp";
+// import { throttle } from "lodash/fp";
+import tippy from "tippy.js";
+// import "tippy.js/dist/tippy.css";
 
-const refPopMap = new Map();
-const refPopperMap = new Map();
 export default {
-  inserted(el, { value }) {
-    // let refHover = 0;
-    // let popHover = 0;
+  inserted(el) {
     const refEle = el.$el || el;
     const popEle = el.nextElementSibling;
 
-    refPopMap.set(refEle, popEle);
     if (!popEle) {
       throw new Error(
         "popper element must be next element with an element with v-popper"
@@ -21,99 +17,26 @@ export default {
       popEle.setAttribute("style", el.getAttribute("popper-style"));
     }
     if (el.getAttribute("popper-class")) {
-      // console.log(el, el.getAttribute("popper-class"));
       popEle.classList.add(el.getAttribute("popper-class"));
     }
-    setTimeout(() => {
-      document.body.append(popEle);
-      instance.scheduleUpdate();
-    }, 500);
 
-    const handleUpdate = ({ instance: { reference, popper } }) => {
-      if (value === "select") {
-        popper.style.width = window.getComputedStyle(reference).width;
-      }
-      instance.scheduleUpdate();
-    };
-    const instance = new Popper(refEle, popEle, {
-      positionFixed: true,
-      placement: value === "bottom-end" ? value : "bottom-start",
-      modifiers: {
-        offset: "10, 10"
-      },
-      onCreate({ instance: { reference, popper } }) {
-        refPopperMap.set(reference, instance);
-        if (value === "select") {
-          popper.style.background = window.getComputedStyle(
-            reference.querySelector("input")
-          ).background;
-          popper.style.width = window.getComputedStyle(reference).width;
-        }
-      },
-      onUpdate: throttle(100, handleUpdate)
+    // const refEleHeight = window.getComputedStyle(refEle).height;
+    tippy(refEle, {
+      content: popEle,
+      // distance: -10,
+      duration: 0,
+      // offset: `0, -1`, //${refEleHeight}`,
+      placement: "bottom-start",
+      flipOnUpdate: true,
+      maxWidth: 10000,
+      trigger: "click",
+      hideOnClick: true,
+      interactive: true,
+      appendTo: document.body
     });
-
-    // popEle.style.visibility = "hidden";
-
-    const show = () => {
-      instance.update();
-      // instance.enableEventListeners();
-      popEle.style.visibility = "visible";
-
-      refEle.removeEventListener("click", show);
-      setTimeout(() => {
-        document.body.addEventListener("click", hide);
-      }, 10);
-    };
-    const hide = () => {
-      // instance.disableEventListeners();
-
-      popEle.style.visibility = "hidden";
-      document.body.removeEventListener("click", hide);
-      setTimeout(() => {
-        if (!refEle.getAttribute("disabled")) {
-          refEle.addEventListener("click", show);
-        }
-      }, 10);
-    };
-
-    hide();
-    // if (!refEle.getAttribute("disabled")) {
-    //   refEle.addEventListener("click", show);
-    // }
-
-    // refEle.addEventListener("mouseenter", () => {
-    //   refHover = 1;
-    //   popEle.style.visibility = "visible";
-    // });
-    // refEle.addEventListener("mouseleave", () => {
-    //   refHover = 0;
-    //   setTimeout(() => {
-    //     if (!popHover && !refHover) {
-    //       popEle.style.visibility = "hidden";
-    //     }
-    //   }, 300);
-    // });
-    // popEle.addEventListener("mouseenter", () => {
-    //   popHover = 1;
-    // });
-    // popEle.addEventListener("mouseleave", () => {
-    //   popHover = 0;
-    //   setTimeout(() => {
-    //     if (!popHover && !refHover) {
-    //       popEle.style.visibility = "hidden";
-    //     }
-    //   }, 300);
-    // });
   },
-  unbind(el) {
-    const popEle = refPopMap.get(el);
-    if (popEle.isConnected) {
-      document.body.removeChild(popEle);
-    } else {
-      popEle.style.display = "none";
-    }
-    const popperInstance = refPopperMap.get(el);
-    popperInstance.destroy();
+
+  unbind({ _tippy: tippyInstance }) {
+    tippyInstance.destroy();
   }
 };
