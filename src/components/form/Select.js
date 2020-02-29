15d08d10d6dabcf253d5727a4228a80c76445fde
lif3ng <lif3ng@icloud.com>
2020-02-29 13:21:25 +0800

    feat: refactor select by tippy.js v-popper

--- src/components/form/Select.js
+++ src/components/form/Select.js
@@ -31,7 +31,10 @@ export default {
   },
   data() {
     return {
-      showText: ""
+      showText: "",
+      inputEl: null,
+      optionsEl: null,
+      tippyInstance: null
     };
   },
   computed: {
@@ -75,6 +78,9 @@ export default {
         this.showText = option[this.labelName] || "";
       }
     },
+    setOptionsAreaSameWidth() {
+      this.optionsEl.style.width = window.getComputedStyle(this.inputEl).width;
+    },
 
     handleSelect(value, label, e) {
       if (this.multiple) {
@@ -93,26 +99,49 @@ export default {
       if (this.value !== value) {
         this.$emit("change", value);
       }
+      this.tippyInstance.hide();
     }
   },
   mounted() {
     if (this.multiple && !this.value) {
       this.$emit("change", []);
     }
-    this.$nextTick(this.renderText);
+    this.inputEl = this.$refs.input.$el;
+    this.optionsEl = this.$refs.optionsArea;
+    this.tippyInstance = this.inputEl._tippy;
+
+    this.optionsEl.style.background = window.getComputedStyle(
+      this.inputEl.querySelector("input")
+    ).background;
+    this.$nextTick(() => {
+      this.renderText();
+      this.tippyInstance.setProps({
+        onShow: () => {
+          this.setOptionsAreaSameWidth();
+        }
+      });
+    });
+    this.resizeFn = () => {
+      this.tippyInstance.hide(0);
+    };
+    window.addEventListener("resize", this.resizeFn);
+  },
+  beforeDestroy() {
+    window.removeEventListener("resize", this.resizeFn);
   },
   render() {
     return (
       <div class={`${prefix}-select`}>
         <Input
-          v-popper="select"
+          ref="input"
+          v-popper={{ a: 3 }}
           attrs={this.$attrs ? this.$attrs.attrs || this.$attrs : {}}
           popper-class={this.$attrs["popper-class"] || ""}
           value={this.showText}
           readonly
           iconAfter="caret-down"
         />
-        <div class={`${prefix}-select-options`}>
+        <div class={`${prefix}-select-options`} ref="optionsArea">
           {this.list.map(
             ({ [this.labelName]: label, [this.valueName]: value }) => (
               <div
