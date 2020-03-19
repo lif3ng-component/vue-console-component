c96d983657f9d7be61813ca9d92e61f0f738d27c
lif3ng <lif3ng@icloud.com>
2020-03-19 11:26:26 +0800

    feat: message z-index, top offset adjust

--- src/components/notice/Message.vue
+++ src/components/notice/Message.vue
@@ -4,10 +4,20 @@ export default {
   data() {
     return {
       message: "",
-      type: "success"
+      type: "success",
+      top: false
     };
   },
   mounted() {
+    try {
+      const isHeaderTop = document
+        .elementFromPoint(1, 1)
+        .closest(`.${this.prefix}-header`);
+      if (!isHeaderTop) {
+        this.top = true;
+      }
+      // eslint-disable-next-line no-empty
+    } catch (e) {}
     setTimeout(() => {
       this.handleClose();
     }, 1500);
@@ -21,7 +31,13 @@ export default {
   },
   render() {
     return (
-      <div class={`${prefix}-message ${prefix}-message-${this.type}`}>
+      <div
+        class={[
+          `${prefix}-message`,
+          `${prefix}-message-${this.type}`,
+          { top: this.top }
+        ]}
+      >
         {this.message}
       </div>
     );
