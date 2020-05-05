7c45df2c34240a434a824f4da83762240904315e
lif3ng <lif3ng@icloud.com>
2020-05-05 20:54:09 +0800

    fix: mock style in util test

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,11 @@ module.exports = {
   preset: "@vue/cli-plugin-unit-jest",
   collectCoverageFrom: ["./src/components/*.{vue,js}"],
   globals: {
-    prefix: "t"
-  }
+    prefix: "t",
+  },
+  moduleNameMapper: {
+    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
+      "<rootDir>/tests/__mocks__/fileMock.js",
+    "\\.css$": "<rootDir>/tests/__mocks__/styleMock.js",
+  },
 };
