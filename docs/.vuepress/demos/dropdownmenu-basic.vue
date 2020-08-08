03dca986663adea3b36cc0bb2dc783b8c92b8fbb
lif3ng <lif3ng@icloud.com>
2020-08-08 19:00:04 +0800

    feat: add 'width', 'align' props, slot block for DropdownMenu

--- docs/.vuepress/demos/dropdownmenu-basic.vue
+++ docs/.vuepress/demos/dropdownmenu-basic.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
-    <nq-dropdown-menu :items="items" />
+    <nq-dropdown-menu :items="items" width="300px">
+      <template #custom>通过 slot 注入的自定义内容</template>
+    </nq-dropdown-menu>
   </div>
 </template>
 <script>
@@ -8,23 +10,26 @@ export default {
   data() {
     return {
       items: [
+        { slot: "custom" },
+        { slot: "custom", slim: true },
         {
+          icon: "edit",
           text: "item1",
-          fn: () => console.log("item1"),
+          fn: () => console.log("item1")
         },
         {
           text: "item2",
-          fn: () => console.log("item1"),
+          fn: () => console.log("item1")
         },
         {
-          split: true,
+          split: true
         },
         {
           text: "group2",
-          fn: () => console.log("group 2"),
-        },
-      ],
+          fn: () => console.log("group 2")
+        }
+      ]
     };
-  },
+  }
 };
 </script>
--- src/components/popper/DropdownMenu.vue
+++ src/components/popper/DropdownMenu.vue
@@ -1,7 +1,17 @@
 <script>
+import Icon from "../Icon";
 export default {
   name: "DropdownMenu",
   props: {
+    width: {
+      type: String
+    },
+    align: {
+      validator: function(value) {
+        return ["left", "right", "center"].indexOf(value) !== -1;
+      },
+      default: "center"
+    },
     items: {
       type: Array,
       required: true
@@ -25,12 +35,32 @@ export default {
   },
   render() {
     return (
-      <div class={`${prefix}-dropdown-menu`} onClick={this.handleHide}>
-        {this.items.map(({ split, text, fn }) =>
-          split ? (
+      <div
+        class={`${prefix}-dropdown-menu`}
+        onClick={this.handleHide}
+        style={{ width: this.width }}
+      >
+        {this.items.map(({ slot, slim, split, text, icon, fn }) =>
+          slot ? (
+            slim ? (
+              this.$slots[slot]
+            ) : (
+              <div
+                class={`${prefix}-dropdown-menu-custom-block`}
+                style={{ textAlign: this.align }}
+              >
+                {this.$slots[slot]}
+              </div>
+            )
+          ) : split ? (
             <div class={`${prefix}-dropdown-menu-split`} />
           ) : (
-            <div class={`${prefix}-dropdown-menu-item`} onClick={fn}>
+            <div
+              class={`${prefix}-dropdown-menu-item`}
+              style={{ textAlign: this.align }}
+              onClick={fn}
+            >
+              {icon && <Icon type={icon} />}
               {text}
             </div>
           )
